89d4753db9f7f1b9ed8e4e9e5b61489878fc1466
JavierQAE <jmolina@pulsellc.com>
2021-11-02 10:30:45 -0700

    Script login orange test webpage

---
+++ Test Suites/Login Orange webpage.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Different login test cases are in this suite</description>
+   <name>Login Orange webpage</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9fc00f0d-f6b6-4215-8c1a-84a64629869b</testSuiteGuid>
+</TestSuiteEntity>
